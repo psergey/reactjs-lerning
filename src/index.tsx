8da4e73de7d4d1851a99384129916ba2b0d54df2
Sergiy Parkhomenko <sergey.parkhomenko@hotmail.com>
2022-09-20 19:20:03 +0300

    converted index.js to typescript

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,7 @@ import { store } from './store';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
     <Provider store={store}>
